95afeae8337f717baa9ae25bab8bd590f557183d
maharianto-dev <aridya.maharianto.dev@gmail.com>
2023-12-31 01:32:33 +0700

    fixing deploy page project selection

--- src/containers/Grid.tsx
+++ src/containers/Grid.tsx
@@ -5,24 +5,24 @@ import { FaMagnifyingGlass, FaCircleXmark } from "react-icons/fa6";
 function Grid({
   projectList,
   showMultiSelect = false,
+  showFilter,
+  onGridLocalProjectListChange,
+  onGridSelectedProjectListChange,
+  localProjectList, // list shown to user
+  selectedGridItems, // selected from localProjectList
 }: {
-  projectList: ProjectModel[];
+  projectList: ProjectModel[] | null;
   showMultiSelect?: boolean;
+  showFilter: boolean;
+  onGridLocalProjectListChange: (state: ProjectModel[]) => void;
+  onGridSelectedProjectListChange: (state: ProjectModel[]) => void;
+  localProjectList: ProjectModel[];
+  selectedGridItems: ProjectModel[];
 }) {
   // let originalProjectList = [] as ProjectModel[];
   // let localProjectList = [] as ProjectModel[];
   // all data
   const [originalProjectList, setOriginalProjectList] = useState(
-    [] as ProjectModel[]
-  );
-
-  // list shown to user
-  const [localProjectList, setLocalProjectList] = useState(
-    [] as ProjectModel[]
-  );
-
-  // selected from localProjectList
-  const [selectedGridItems, setSelectedGridItems] = useState(
     [] as ProjectModel[]
   );
 
@@ -32,11 +32,16 @@ function Grid({
 
   useEffect(() => {
     if (projectList) {
-      setSelectedGridItems([] as ProjectModel[]);
+      projectList.sort((row1, row2) => {
+        const row1Name = row1.project_name;
+        const row2Name = row2.project_name;
+        if (row1Name < row2Name) return -1;
+        if (row1Name > row2Name) return 1;
+        return 0;
+      });
+      onGridSelectedProjectListChange([] as ProjectModel[]);
       setOriginalProjectList([...projectList]);
-      setLocalProjectList([...projectList]);
-      // originalProjectList = [...projectList];
-      // localProjectList = [...originalProjectList];
+      onGridLocalProjectListChange([...projectList]);
 
       resetState();
     }
@@ -45,14 +50,15 @@ function Grid({
   useEffect(() => {
     const doFilter = setTimeout(() => {
       if (searchFilter == null || searchFilter.trim() === "") {
-        setLocalProjectList([...originalProjectList]);
+        onGridLocalProjectListChange([...originalProjectList]);
       } else {
-        setLocalProjectList([
+        onGridLocalProjectListChange([
           ...originalProjectList.filter((el) =>
             el.project_name.toLowerCase().includes(searchFilter.toLowerCase())
           ),
         ]);
       }
+      onGridSelectedProjectListChange([...[]]);
     }, 1000);
 
     return () => clearTimeout(doFilter);
@@ -67,19 +73,22 @@ function Grid({
     event: BaseSyntheticEvent,
     row_data: ProjectModel
   ) => {
+    let currentSelectedGridItems = [...selectedGridItems];
     if (event.target.checked === true) {
-      setSelectedGridItems((currVal: any) => [...currVal, row_data]);
+      currentSelectedGridItems = [...currentSelectedGridItems, row_data];
+      onGridSelectedProjectListChange(currentSelectedGridItems);
     } else {
-      setSelectedGridItems((currVal) =>
-        currVal.filter((el) => el.project_id !== row_data.project_id)
+      currentSelectedGridItems = currentSelectedGridItems.filter(
+        (el) => el.project_id !== row_data.project_id
       );
+      onGridSelectedProjectListChange(currentSelectedGridItems);
     }
   };
 
   const handleIsSelectedAllChange = (event: BaseSyntheticEvent) => {
-    setSelectedGridItems([...([] as ProjectModel[])]);
+    onGridSelectedProjectListChange([...([] as ProjectModel[])]);
     if (event.target.checked === true) {
-      setSelectedGridItems([...localProjectList]);
+      onGridSelectedProjectListChange([...localProjectList]);
     }
   };
 
@@ -109,13 +118,13 @@ function Grid({
   };
 
   return (
-    <div className="overflow-x-auto overflow-y-auto min-h-full max-h-full">
+    <div className="overflow-x-auto overflow-y-auto">
       <table className="table table-pin-rows">
         <thead>
           <tr className="bg-indigo-600">
             {showMultiSelect && (
-              <th>
-                <div>
+              <th className="w-0">
+                <div className="w-full">
                   <input
                     type="checkbox"
                     className="checkbox checkbox-accent"
@@ -129,13 +138,20 @@ function Grid({
               <div className="flex flex-col">
                 <div className="flex flex-row items-center">
                   <span className="mr-2">Project Name</span>{" "}
-                  <button onClick={onHeaderClick}>
-                    {isShowingFilter === true ? (
-                      <FaCircleXmark />
-                    ) : (
-                      <FaMagnifyingGlass />
-                    )}
-                  </button>
+                  {showFilter === true && (
+                    <div className="mr-2 w-full flex flex-row">
+                      <button className="mr-2" onClick={onHeaderClick}>
+                        {isShowingFilter === true ? (
+                          <FaCircleXmark />
+                        ) : (
+                          <FaMagnifyingGlass />
+                        )}
+                      </button>
+                      {!(searchFilter == null || searchFilter.trim() === "") ? (
+                        <p>Filter applied</p>
+                      ) : null}
+                    </div>
+                  )}
                 </div>
                 {isShowingFilter === true && (
                   <h1 className="flex-1">
--- src/containers/GridOperator.tsx
+++ src/containers/GridOperator.tsx
@@ -1,17 +1,51 @@
-function GridOperator() {
+import { GridOperatorTypeEnum } from "../models/GridOperatorModel";
+
+function GridOperator({
+  onHandleGridButtonEvent,
+}: {
+  onHandleGridButtonEvent: (gridOperatorType: GridOperatorTypeEnum) => void;
+}) {
   return (
     <div className="flex flex-col justify-center bg-blue-600 min-h-full max-h-full gap-12">
       <div className="flex flex-row justify-center">
-        <button className="btn btn-secondary aspect-square">a</button>
+        <button
+          className="btn btn-secondary aspect-auto w-5/6"
+          onClick={() =>
+            onHandleGridButtonEvent(GridOperatorTypeEnum.MoveAllRight)
+          }
+        >
+          Move All Right
+        </button>
       </div>
       <div className="flex flex-row justify-center">
-        <button className="btn btn-secondary aspect-square">b</button>
+        <button
+          className="btn btn-secondary aspect-auto w-5/6"
+          onClick={() =>
+            onHandleGridButtonEvent(GridOperatorTypeEnum.MoveSelectedRight)
+          }
+        >
+          Move Selected Right
+        </button>
       </div>
       <div className="flex flex-row justify-center">
-        <button className="btn btn-secondary aspect-square">c</button>
+        <button
+          className="btn btn-secondary aspect-auto w-5/6"
+          onClick={() =>
+            onHandleGridButtonEvent(GridOperatorTypeEnum.MoveSelectedLeft)
+          }
+        >
+          Move Selected Left
+        </button>
       </div>
       <div className="flex flex-row justify-center">
-        <button className="btn btn-secondary aspect-square">d</button>
+        <button
+          className="btn btn-secondary aspect-auto w-5/6"
+          onClick={() =>
+            onHandleGridButtonEvent(GridOperatorTypeEnum.MoveAllLeft)
+          }
+        >
+          Move All Left
+        </button>
       </div>
     </div>
   );
